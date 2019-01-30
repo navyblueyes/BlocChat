303ff099eceb2903b74ab9f17217bf3f9c92208a
modelwifeofamerica <modelwifeofamerica@gmail.com>
2019-01-30 13:16:50 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import * as firebase from "firebase";
+import RoomList from './components/RoomList.js';
+
+var config = {
+    apiKey: "AIzaSyBGw5gpAgVb-revSzOZn439t9vspIUV88s",
+    authDomain: "blocchat-mink.firebaseapp.com",
+    databaseURL: "https://blocchat-mink.firebaseio.com",
+    projectId: "blocchat-mink",
+    storageBucket: "blocchat-mink.appspot.com",
+    messagingSenderId: "700330258369"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <div className="App">
+          <div className="titleRooms">Available Rooms</div>
+          <div className="listOfRooms">
+            <RoomList
+              setActiveRoom={this.setActiveRoom}
+              firebase={firebase}
+            />
+          </div>
+          </div>
+      </React.Fragment>
     );
   }
 }
---
+++ src/components/RoomList.js
@@ -0,0 +1,102 @@
+import React, { Component } from "react";
+import App from "./../App.js";
+
+
+class RoomList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rooms: [],
+      newRoomName: "",
+      roomId: ""
+    };
+    this.roomsRef = this.props.firebase.database().ref("rooms");
+  }
+
+  componentDidMount() {
+    this.roomsRef.on("child_added", snapshot => {
+      const room = snapshot.val();
+      room.key = snapshot.key;
+      this.setState({ rooms: this.state.rooms.concat(room) });
+    });
+    this.roomsRef.on("child_added", snapshot => {
+      const room = snapshot.val();
+      room.key = snapshot.key;
+      this.setState({ rooms: this.state.rooms.concat(room) });
+      if (this.state.rooms.length === 1) {
+        this.props.setActiveRoom(room);
+      }
+    });
+    this.roomsRef.on("child_removed", snapshot => {
+      this.setState({
+        rooms: this.state.rooms.filter(room => room.key !== snapshot.key)
+      });
+    });
+    console.log(this.roomsRef);
+  }
+
+  createRooms(newRoomName) {
+    this.roomsRef.push({
+      name: newRoomName
+    });
+  }
+
+  deleteRoom = roomName => {
+    console.log("Delete Room Triggered");
+    const newRoomsArray = [];
+    this.state.rooms.map((room, i) => {
+      if (room.key != roomName.key) {
+        newRoomsArray.push(room);
+      }
+    });
+    this.setState({ rooms: newRoomsArray });
+    console.log(this.state.rooms);
+  };
+
+  handleChange(e) {
+    this.setState({ newRoomName: e.target.value });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="available-rooms">
+          {this.state.rooms.map((room, i) => (
+            <a>
+              <p key={i} onClick={() => this.props.setActiveRoom(room)}>
+                Room name: {room.name}
+              </p>
+              <input
+                className="delete-room"
+                type="button"
+                value="Delete this room"
+                onClick={e => this.deleteRoom(room)}
+              />
+            </a>
+          ))}
+        </div>
+
+        <div className="new-room-form">
+          <h1>Add a new chatroom!</h1>
+          <form
+            onSubmit={e => {
+              e.preventDefault();
+              this.createRooms(this.state.newRoomName);
+            }}
+          >
+            <label for="roomName">Room Name: </label>
+            <input
+              type="text"
+              id="roomName"
+              value={this.state.newRoomName}
+              onChange={e => this.handleChange(e)}
+            />
+            <input type="submit" />
+          </form>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default RoomList;
